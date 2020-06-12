b2c2264f48ab33566bcd2577bc23d6801b109780
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-12 10:19:12 -0700

    Complete behaviors 2 add 3 with and if else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 // Bussiness logic
 // function translation(num) {
-//   const numbers = [1,2,3];
 //   const vocab = [
 //     "Beep!",
 //     "Boop!",
@@ -9,19 +8,22 @@
 //     ]
 //   }
 function rodgers (num) {
-  const acceptable = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  accceptable = [1,2,3]
   const vocab = [
     "Beep!",
     "Boop!",
     "Won't you be my neighbor?",
 
     ]
-    
-    if (!num.includes(acceptable)) {
-      alert("unacceptable")
-    } eslse if (num.includes(acceptable[4]))
-      output3 = vocab[2];
-  };
+  if (isNaN(num)) {
+    alert("thats not a number");
+  } else if (num === "2" ) {
+    alert(vocab[1])
+  } else if (num === "1" ) {
+    alert(vocab[0])
+  } 
+
+}
 
 // UI logic
 $(document).ready(function() {
