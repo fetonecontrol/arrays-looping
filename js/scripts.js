5536bdcdfe0f8fed7187eb9edfa7427c5ba62f7a
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-12 10:49:18 -0700

    begin working on final proticol, split user input into an array as numbers

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@
 //   }
 function rodgers (num) {
   accceptable = [1,2,3]
+  num1 = parseInt(num)
   const vocab = [
     "Beep!",
     "Boop!",
@@ -17,13 +18,18 @@ function rodgers (num) {
     ]
   if (isNaN(num)) {
     alert("thats not a number");
-  } else if (num === "2" ) {
+  } else if (num1 === 3 ) {
+    alert(vocab[2])
+  } else if (num1 === 2 ) {
     alert(vocab[1])
-  } else if (num === "1" ) {
+  } else if (num1 === 1) {
     alert(vocab[0])
-  } 
-
+  } else if (num1 > 3) {
+    numSplit2 = parseInt(num);
+    numSplit2 = num.split('');
+  }
 }
+
 
 // UI logic
 $(document).ready(function() {
