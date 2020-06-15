3693b45c6a589f55e7256f482fdd10880ff7c4e4
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-15 08:46:12 -0700

    clean up and refactory code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 function rodgers(num) {
   let num1 = parseInt(num);
-  const trigger = ["1", "2", "3",];
   let numArray = [];
   const vocab = [
     "Beep!",
@@ -27,61 +26,24 @@ function rodgers(num) {
       console.log(numArray);
     for (let i=0; i <=numIntiger; i++) {
       if (((i -3) % 10) === 0){
-      numArray.push(vocab[2]);
+        numArray.push(vocab[2]);
       } else if (((i -2) % 10) === 0) {
-      numArray.push(vocab[1]);
+        numArray.push(vocab[1]);
       } else if (((i -1) % 10) === 0) {
         numArray.push(vocab[0]);
       } else {
-      numArray.push(i);
+        numArray.push(i);
       }
-      // stringArray = numArray.toString();
-      // stringArray2 = stringArray.split("3").join(vocab[2]);
-      //stringArray3 = stringArray2.split("2").join(vocab[1]);
-    }
-      // if (trigger.indexOf(stringArray.charAt(i)) >= 0)  {
-      //   stringArray.push("beep");      }
-
-
-      //stringArray3 = stringArray2.split("2").join(vocab[1]);
   }
     console.log(numArray);
     return numArray;
 }
-// function sub(num){
-//   let arr2 = num;
-//   let valid = ["1", "2", "3",];
-//   const vocab = [
-//     "Beep!",
-//     "Boop!",
-//     "Won't you be my neighbor?",
-
-//     ];
-  
-//   }
-    // if (valid.indexOf(num[0]) > -1) {
-    //   arr2 = num.splice(vocab[0]);
-    //   return arr2;
-
-    // }
-
-//     if (vowels.indexOf(str[0]) > -1) {
-//       newStr = str + "way";
-//       return newStr;
-//   } else {
-//       let firstMatch = str.match(/[aeiou]/g) || 0;
-//       let vowel = str.indexOf(firstMatch[0]);
-//       newStr = str.substring(vowel) + str.substring(0, vowel) + "ay";
-//       return newStr;
-// }
-
-// UI logic
+}
 $(document).ready(function() {
   $("form#form1").submit(function(event) {
     event.preventDefault();
     let number = $("input#number").val();
     let number2 = rodgers(number);
-    //let number3 = sub(number2)
     $("#output").show();
     $("#result").text(number2);
   });
